c449603be8c832ab7f23f5658078a0bedb5e5b13
i2305 <iva.vukovic695@gmail.com>
2023-09-08 17:33:21 +0200

    add javaScript file

---
+++ script.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded", function(){
+    
+    let password=document.querySelector("#password").value;
+    let confirmPassword=document.querySelector("#confirmPassword").value;
+    let error=document.querySelector("#error");
+
+    if (password == "" && confirmPassword == ""){
+        error.textContent="*Passwords do not match";
+    }
+    else if (password != confirmPassword){
+        error.textContent="*Passwords do not match";
+    }
+    else if (password == confirmPassword){
+        error.textContent="";
+    }
+
+});
